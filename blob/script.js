9f775a2a8a2322587b568dbda2f7a2962bb3d70b
mynguyen231 <42570130+mynguyen231@users.noreply.github.com>
2022-06-23 11:19:35 +0200

    Add files via upload

---
+++ blob/script.js
@@ -0,0 +1,47 @@
+const docStyle = document.documentElement.style;
+const threshold = [...Array(100).keys()].map(i => i / 100);
+
+const itemsObserver = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting) {
+        document.documentElement.style.setProperty(
+          "--translate-value",
+          Math.round(-(1 - entry.intersectionRatio) * 40)
+        );
+        document.documentElement.style.setProperty(
+          "--translate-value-header",
+          Math.round(-(entry.intersectionRatio * 90))
+        );
+      }
+    });
+  },
+  { threshold: threshold }
+);
+
+const itemsObserverSecondary = new IntersectionObserver(
+  entries => {
+    entries.forEach(entry => {
+      if (entry.isIntersecting && entry.intersectionRatio > 0) {
+        entry.target.classList.remove("toggle-class");
+        itemsObserverSecondary.unobserve(entry.target);
+      }
+    });
+  },
+  { threshold: threshold }
+);
+
+itemsObserver.observe(document.querySelector(".main"));
+const toggles = [...document.querySelectorAll(".toggle-class")];
+toggles.forEach(toggle => {
+  itemsObserverSecondary.observe(toggle);
+})
+
+function myFunction() {
+  var x = document.getElementById("nav-mobile-collapse");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
